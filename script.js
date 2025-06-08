1f3c104552238329af7c324cd4da9f03043c39e2
harmansaini4tech <harmansaini10000@gmail.com>
2025-06-08 10:15:47 +0530

    added the toogle dark and light mode

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ document.querySelectorAll(".navbar-nav .nav-link").forEach((navLink) => {
     }
   });
 });
+
+const toogleDarkLight = document.getElementById("toogleDarkLight");
+const sunMoonIcons = document.getElementsByTagName("#toogleDarkLight i");
+const body = document.getElementById("mainBody");
+
+toogleDarkLight.addEventListener("click", () => {
+  const currentAtr = body.getAttribute("data-bs-theme");
+  const newTheme = currentAtr === "light" ? "dark" : "light";
+  body.setAttribute("data-bs-theme", newTheme);
+  const element = document.getElementById("sunAndmoonIcon");
+
+  if (element.classList == "ri-sun-fill") {
+    element.classList.remove("ri-sun-fill");
+    element.classList.add("ri-moon-fill");
+  } else {
+    element.classList.remove("ri-moon-fill");
+    element.classList.add("ri-sun-fill");
+  }
+});
